c1aa80269a2f1deb28c25d9753aed0509c46b132
BISHAL BHANDARI <80243759+Bishal-Bhandari01@users.noreply.github.com>
2022-09-16 18:55:31 +0545

    Sticky top on nav

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Footer from "./footer";
 export default function Home() {
   return (
     <div>
-      <div>
+      <div className="sticky-top">
        <NavBar />
       </div>
       <Main />
